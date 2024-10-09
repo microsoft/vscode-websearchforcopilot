c7f98dcdaf02f1804f71cfb26122146eb6aa99f7
andreamah <andrea_mah22@hotmail.com>
2024-10-09 08:48:07 -0700

    organize embeddings file

--- src/scraper/embeddings.ts
+++ src/scraper/embeddings.ts
@@ -4,6 +4,10 @@ import { isUnexpected } from "@azure-rest/ai-inference";
 import ModelClient from "@azure-rest/ai-inference";
 import { AzureKeyCredential } from "@azure/core-auth";
 
+export type Embedding = readonly number[];
+type FileChunkWithEmbeddings = [FileChunk, Embedding];
+const modelName = "text-embedding-3-small";
+const endpoint = "https://models.inference.ai.azure.com";
 
 export class EmbeddingsIndex {
     chunks: FileChunk[] = [];
@@ -13,87 +17,81 @@ export class EmbeddingsIndex {
     }
 
     public async search(query: string, maxResults = 5, minThreshold = -Infinity): Promise<FileChunk[]> {
-        return rankChunks(query,this.chunks,maxResults);
-    }
-}
-
-async function rankChunks(query: string, chunks: FileChunk[],maxResults:number): Promise<FileChunk[]>{
-
-    const stringsToFetch = [
-        query,
-        ...chunks.map(chunk => chunk.text)
-    ];
-    const embeddings = await getEmbeddings(stringsToFetch);
-
-    
-    const fileChunksWithEmbeddings: FileChunkWithEmbeddings[] = [];
-    const queryEmbedding = embeddings[0];
-    let i= 1;
-    for (const e of embeddings) {
-        fileChunksWithEmbeddings.push([chunks[i], e]);
-        i++;
+        return this.rankChunks(query, this.chunks, maxResults);
     }
 
-    const ranked = rankEmbeddings<FileChunk>(queryEmbedding,fileChunksWithEmbeddings, maxResults);
-    return ranked;
+    async rankChunks(query: string, chunks: FileChunk[], maxResults: number): Promise<FileChunk[]> {
 
-}
-
-export type Embedding = readonly number[];
-type FileChunkWithEmbeddings = [FileChunk, Embedding];
-const modelName = "text-embedding-3-small";
-const endpoint = "https://models.inference.ai.azure.com";
+        const stringsToFetch = [
+            query,
+            ...chunks.map(chunk => chunk.text)
+        ];
+        const embeddings = await this.getEmbeddings(stringsToFetch);
 
 
-export async function getEmbeddings(text: string[]): Promise<Embedding[]> {
-    const githubSession = await vscode.authentication.getSession('github', ['user:email'], { createIfNone: true });
-
-
-    const client = ModelClient(endpoint, new AzureKeyCredential(githubSession.accessToken));
-
-
-    const sendReq = async (subset: string[]) => {
-        const response = await client.path("/embeddings").post({
-            body: {
-                input: subset,
-                model: modelName
-            }
-        });
-        
-
-        if (isUnexpected(response)) {
-            throw response.body.error;
+        const fileChunksWithEmbeddings: FileChunkWithEmbeddings[] = [];
+        const queryEmbedding = embeddings[0];
+        let i = 1;
+        for (const e of embeddings) {
+            fileChunksWithEmbeddings.push([chunks[i], e]);
+            i++;
         }
 
-        return response.body.data.map(d => d.embedding);
-    };
+        const ranked = this.rankEmbeddings<FileChunk>(queryEmbedding, fileChunksWithEmbeddings, maxResults);
+        return ranked;
 
-    const inputs = Array.from(text);
-    const ret = [];
-    while (inputs.length > 0) {
-        const subset = inputs.splice(0,8);
-        ret.push(...await sendReq(subset));
     }
-    return ret;
-}
+
+    async getEmbeddings(text: string[]): Promise<Embedding[]> {
+        const githubSession = await vscode.authentication.getSession('github', ['user:email'], { createIfNone: true });
 
 
-function rankEmbeddings<T>(
-    queryEmbedding: Embedding,
-    items: ReadonlyArray<readonly [T, Embedding]>,
-    maxResults: number,
-): T[] {
-    const minThreshold = 0.0;
-    return items
-        .map(([item, embedding]): { readonly score: number; readonly item: T } => {
-            let dotProduct = 0;
-            for (let i = 0; i < embedding.length; i++) {
-                dotProduct += embedding[i] * queryEmbedding[i];
+        const client = ModelClient(endpoint, new AzureKeyCredential(githubSession.accessToken));
+
+
+        const sendReq = async (subset: string[]) => {
+            const response = await client.path("/embeddings").post({
+                body: {
+                    input: subset,
+                    model: modelName
+                }
+            });
+
+
+            if (isUnexpected(response)) {
+                throw response.body.error;
             }
-            return { score: dotProduct, item: item };
-        })
-        .filter(entry => entry.score > minThreshold)
-        .sort((a, b) => b.score - a.score)
-        .slice(0, maxResults)
-        .map(entry => entry.item);
+
+            return response.body.data.map(d => d.embedding);
+        };
+
+        const inputs = Array.from(text);
+        const ret = [];
+        while (inputs.length > 0) {
+            const subset = inputs.splice(0, 8);
+            ret.push(...await sendReq(subset));
+        }
+        return ret;
+    }
+
+    rankEmbeddings<T>(
+        queryEmbedding: Embedding,
+        items: ReadonlyArray<readonly [T, Embedding]>,
+        maxResults: number,
+    ): T[] {
+        const minThreshold = 0.0;
+        return items
+            .map(([item, embedding]): { readonly score: number; readonly item: T } => {
+                let dotProduct = 0;
+                for (let i = 0; i < embedding.length; i++) {
+                    dotProduct += embedding[i] * queryEmbedding[i];
+                }
+                return { score: dotProduct, item: item };
+            })
+            .filter(entry => entry.score > minThreshold)
+            .sort((a, b) => b.score - a.score)
+            .slice(0, maxResults)
+            .map(entry => entry.item);
+    }
 }
+
