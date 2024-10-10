3c12f686ee8c26a298363a07405dc0d4cb8bef6e
Tyler Leonhardt <tyleonha@microsoft.com>
2024-10-10 12:56:38 -0700

    fix tsx compilation

--- webpack.config.js
+++ webpack.config.js
@@ -25,12 +25,12 @@ const extensionConfig = {
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js', '.tsx']
   },
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.([cm]?ts|tsx)$/,
         exclude: /node_modules/,
         use: [
           {
