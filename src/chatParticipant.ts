3cb6ad070ec8123c9629c2c4cd8e872eef9fac66
Tyler Leonhardt <me@tylerleonhardt.com>
2024-10-16 15:48:51 -0700

    Enable tracing

--- src/chatParticipant.ts
+++ src/chatParticipant.ts
@@ -3,11 +3,11 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 import { ExtensionContext, ChatRequestHandler, ChatRequest, ChatContext, ChatResponseStream, CancellationToken, lm, LanguageModelChatRequestOptions, chat, ThemeIcon, LanguageModelTextPart, LanguageModelToolCallPart, LanguageModelChatTool, Uri, l10n } from "vscode";
-import { renderPrompt } from '@vscode/prompt-tsx';
 import { ToolUserPrompt } from "./chatToolPrompt";
 import { WebSearchTool } from "./chatTool";
 import Logger from "./logger";
 import chatState from "./chatState";
+import { renderPrompt } from "./promptTracing";
 
 class WebSearchChatParticipant {
     constructor(private readonly _context: ExtensionContext) { }
@@ -32,7 +32,30 @@ class WebSearchChatParticipant {
         const options: LanguageModelChatRequestOptions = {
             justification: l10n.t('To analyze web search results and summarize an answer'),
         };
-        let { messages, references } = await renderPrompt(ToolUserPrompt, { context, request, toolCalls: [] }, { modelMaxPromptTokens: model.maxInputTokens }, model);
+
+        let { messages, references } = await renderPrompt({ modelMaxPromptTokens: model.maxInputTokens }, ToolUserPrompt, { context, request, toolCalls: [] }, model, token);
+        // const renderer = new PromptRenderer({ modelMaxPromptTokens: model.maxInputTokens }, ToolUserPrompt, { context, request, toolCalls: [] }, new AnyTokenizer(model.countTokens));
+        // let result: RenderPromptResult;
+        // if (this._context.extensionMode === ExtensionMode.Development) {
+        //     const tracer = new HTMLTracer();
+        //     renderer.tracer = tracer;
+        //     result = await renderer.render(undefined, token);
+        //     const server = await tracer.serveHTML();
+        //     await window.withProgress({ location: ProgressLocation.Notification, title: `Tracer server running at ${server.address}`, cancellable: true }, async (progress, token) => {
+        //         await new Promise<void>(resolve => {
+        //             const dispose = token.onCancellationRequested(() => {
+        //                 dispose.dispose();
+        //                 server.dispose();
+        //                 resolve();
+        //             });
+        //         });
+        //     });
+        // } else {
+        //     result = await renderer.render(undefined, token);
+        // }
+        // let messages = toVsCodeChatMessages(result.messages);
+        // let references = result.references;
+        // let { messages, references } = await renderPrompt(ToolUserPrompt, { context, request, toolCalls: [] }, { modelMaxPromptTokens: model.maxInputTokens }, model);
 
         // Put our tool at the very end so that it processes the search query after resolving all other variables
         const toolReferences = [...request.toolReferences.filter(ref => ref.id === WebSearchTool.ID), { id: WebSearchTool.ID }];
@@ -62,13 +85,35 @@ class WebSearchChatParticipant {
             if (toolCalls.length) {
                 // Store the stream so that the tool can reference it
                 chatState.set(request.toolInvocationToken, stream);
-                const result = await renderPrompt(
-                    ToolUserPrompt,
-                    { context, request, toolCalls },
-                    { modelMaxPromptTokens: model.maxInputTokens },
-                    model);
+                const result = await renderPrompt({ modelMaxPromptTokens: model.maxInputTokens }, ToolUserPrompt, { context, request, toolCalls }, model, token);
                 messages = result.messages;
                 references = result.references;
+                // const renderer = new PromptRenderer({ modelMaxPromptTokens: model.maxInputTokens }, ToolUserPrompt, { context, request, toolCalls: [] }, new AnyTokenizer(model.countTokens));
+                // let result: RenderPromptResult;
+                // if (this._context.extensionMode === ExtensionMode.Development) {
+                //     const tracer = new HTMLTracer();
+                //     renderer.tracer = tracer;
+                //     result = await renderer.render(undefined, token);
+                //     const server = await tracer.serveHTML();
+                //     await window.withProgress({ location: ProgressLocation.Notification, title: `Tracer server running at ${server.address}`, cancellable: true }, async (progress, token) => {
+                //         await new Promise<void>(resolve => {
+                //             const dispose = token.onCancellationRequested(() => {
+                //                 dispose.dispose();
+                //                 server.dispose();
+                //                 resolve();
+                //             });
+                //         });
+                //     });
+                // } else {
+                //     result = await renderer.render(undefined, token);
+                // }
+                // messages = toVsCodeChatMessages(result.messages);
+                // references = result.references;
+                // const result = await renderPrompt(
+                //     ToolUserPrompt,
+                //     { context, request, toolCalls },
+                //     { modelMaxPromptTokens: model.maxInputTokens },
+                //     model);
 
                 // RE-enter
                 return runWithFunctions();
